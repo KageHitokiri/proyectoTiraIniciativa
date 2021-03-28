25d4f5f4bb680c20bf9499ef2606b86ec766b783
Carlos <skhcri@gmail.com>
2021-03-28 07:25:35 +0200

    Preparación json para post dinámicos

--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
 'use strict'
 
-window.addEventListener('load',()=>{
-    console.log("Working");
+$(document).ready(()=>{
+    /**
+     * Post
+     */
+    let posts = [
+        {
+            title: 'Prueba de título 1',
+            date: "Publicado el " + moment().format('dddd') +" "+ + moment().date()+ "/"+ moment().format('MMMM')+"/"+ moment().format('YYYY'),
+            content: '1Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt odio dolore nulla neque, vitae ipsum corporis, maiores beatae reprehenderit nisi soluta illum possimus? Delectus adipisci perspiciatis quidem, modi error aut.'
+        },        {
+            title: 'Prueba de título 2',
+            date: "Publicado el " + moment().format('dddd') +" "+ + moment().date()+ "/"+ moment().format('MMMM')+"/"+ moment().format('YYYY'),
+            content: '2Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt odio dolore nulla neque, vitae ipsum corporis, maiores beatae reprehenderit nisi soluta illum possimus? Delectus adipisci perspiciatis quidem, modi error aut.'
+        },        {
+            title: 'Prueba de título 3',
+            date: "Publicado el " + moment().format('dddd') +" "+ + moment().date()+ "/"+ moment().format('MMMM')+"/"+ moment().format('YYYY'),
+            content: '3Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt odio dolore nulla neque, vitae ipsum corporis, maiores beatae reprehenderit nisi soluta illum possimus? Delectus adipisci perspiciatis quidem, modi error aut.'
+        },        {
+            title: 'Prueba de título 4',
+            date: "Publicado el " + moment().format('dddd') +" "+ + moment().date()+ "/"+ moment().format('MMMM')+"/"+ moment().format('YYYY'),
+            content: '4Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt odio dolore nulla neque, vitae ipsum corporis, maiores beatae reprehenderit nisi soluta illum possimus? Delectus adipisci perspiciatis quidem, modi error aut.'
+        },        {
+            title: 'Prueba de título 5',
+            date: "Publicado el " + moment().format('dddd') +" "+ + moment().date()+ "/"+ moment().format('MMMM')+"/"+ moment().format('YYYY'),
+            content: '5Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt odio dolore nulla neque, vitae ipsum corporis, maiores beatae reprehenderit nisi soluta illum possimus? Delectus adipisci perspiciatis quidem, modi error aut.'
+        }
+    ];
 
+    console.log(posts);
 })
